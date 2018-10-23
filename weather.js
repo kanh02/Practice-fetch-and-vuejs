7f49a9873dc61c869458d71ddec0d1e2f1658163
oscar <kajiryohji02@gmail.com>
2018-10-23 13:35:27 +0200

    refactory complete

--- weather.js
+++ weather.js
@@ -2,14 +2,14 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		city: {},
-		one:'',
-		cityname:'',
-		tempmmin:'',
-		tempmmax:'',
-		max:'',
-		min:'',
-		weat:'',
-		mensaje:'',
+		one: '',
+		cityname: '',
+		tempmmin: '',
+		tempmmax: '',
+		max: '',
+		min: '',
+		weat: '',
+		mensaje: '',
 		selected: '',
 		humy: '',
 		options: [
@@ -28,7 +28,7 @@ var app = new Vue({
 				text: 'Soupe a L Oignon'
 			},
 			{
-				city: 'Port',
+				city: 'Porto',
 				image: '',
 				text: 'Francesinhas'
 			},
@@ -71,26 +71,26 @@ var app = new Vue({
 
 	},
 	methods: {
-		
-	
+
+
 		getCity: function () {
-		
+
 			var url = "https://api.openweathermap.org/data/2.5/forecast?q=";
-			var city = "London"; 
+			var city = "London";
 			var key = "&APPID=541d06d219cbd104c02d1777edb4561b";
-			
-		
+
+
 			this.one = document.formulario.cities.value;
 			console.log(this.one)
-			city = this.one
-
-			console.log(city)
-			console.log(this.one)
+			city = this.one;
+			if(city != null){
+				city ="London";
+			}
 			
 
-			
 
-			fetch(url+city+key, {
+
+			fetch(url + city + key, {
 				method: "GET",
 
 			}).then(function (response) {
@@ -101,63 +101,54 @@ var app = new Vue({
 			}).then(function (json) {
 				var data = json;
 				app.city = data;
-				app.cityname= data.city.name;
+				console.log(app.city)
 				
+				app.cityname = data.city.name;
+
 				app.humy = data.list[0].main.humidity;
-				
-				app.tempmmax=data.list[0].main.temp_max;
-				app.tempmmin=data.list[0].main.temp_min;
-				
+
+				app.tempmmax = data.list[0].main.temp_max;
+				app.tempmmin = data.list[0].main.temp_min;
+
 				app.grades(app.tempmmax);
 				app.grades(app.tempmmin);
+				
 				app.max = app.grades(app.tempmmax);
 				app.min = app.grades(app.tempmmin);
-				
-				
+
 				app.weather(app.max);
 				app.mensaje = app.weather(app.max);
-				console.log(app.mensaje);
-				
 
-
-				console.log(data)
-				console.log(app.city)
 
 			}).catch(function (error) {
 				console.log("Request failed: " + error.message);
 			})
 		},
-		
-						grades: function (array) {
-							
-							var kel = -273.15;
-							var tempfinal = array + kel;
-							var temp = tempfinal.toFixed();
-							return (temp)
-							
+
+		grades: function (array) {
+
+			var kel = -273.15;
+			var tempfinal = array + kel;
+			var temp = tempfinal.toFixed();
+			return (temp)
+
 		},
-							
-						weather: function(array){
-							
-							var mesage1 ="temperature is sunny";
-							var mesage2 ="temperature is cold";
-							var vacio="";
-							if( array > 15){
-								vacio = mesage1;
-							}else if( array < 12){
-								vacio = mesage2;
-							}
-							console.log(vacio)
-							console.log(array)
-							
-							return (vacio)
-						}
-		
-		
-	
+
+		weather: function (array) {
+
+			var mesage1 = "temperature is sunny";
+			var mesage2 = "temperature is cold";
+			var vacio = "";
+			if (array > 15) {
+				vacio = mesage1;
+			} else if (array < 15) {
+				vacio = mesage2;
+			}
+
+			return (vacio)
+		}
 	}
 
 })
 
 //541d06d219cbd104c02d1777edb4561b
-
